7cca41408be3b2d93c1a03f8ab5ef85acb0366d7
Adam Russak <adamrussak@gmail.com>
2022-11-11 15:47:57 +0200

    [go-release-mamnger-test][sdfs1c8f12be0e6b99e8cfbea8118419b983df0][2: keepiong on working]

--- struct.go
+++ struct.go
@@ -1,6 +1,12 @@
 package main
 
-type commit struct{
-	Hash string
+type commit struct {
+	Hash    string
 	Comment string
+}
+
+type workItem struct {
+	Name        string
+	ServiceName string
+	Hash        string
 }
