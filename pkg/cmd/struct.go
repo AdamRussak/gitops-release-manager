aba8ba86f6e60b40cd83bd1c3ee160d5c1563736
Adam Russak <adamrussak@gmail.com>
2022-11-14 00:32:28 +0200

    fixed my stupid mistacke2

---
+++ pkg/cmd/struct.go
@@ -0,0 +1,12 @@
+package cmd
+
+type FlagsOptions struct {
+	Output       string
+	CommitHash   string
+	Orgenization string
+	Pat          string
+	Project      string
+	RepoPath     string
+	DryRun       bool
+	Gitpush      bool
+}
