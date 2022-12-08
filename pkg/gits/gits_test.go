1d3085610264c28a26beeee74d719e964fec3eec
AdamRussak <adamrussak@gmail.com>
2022-12-08 21:07:58 +0000

    working on unitesting

--- pkg/gits/gits_test.go
+++ pkg/gits/gits_test.go
@@ -6,9 +6,19 @@ import (
 )
 
 func TestIsVersionTag(t *testing.T) {
-	testTag := "1.0.0"
-	testIsVersioning := isVersionTag(testTag)
-	if !testIsVersioning {
-		t.Fatalf(`isVersionTag(%s) = %s,should have been %s`, testTag, fmt.Sprint(testIsVersioning), "true")
-	}
+	t.Run("Good Info", func(t *testing.T) {
+		testTag := "1.0.0"
+		testIsVersioning := isVersionTag(testTag)
+		if !testIsVersioning {
+			t.Fatalf(`isVersionTag(%s) = %s,should have been %s`, testTag, fmt.Sprint(testIsVersioning), "true")
+		}
+	})
+
+	t.Run("Bad Info", func(t *testing.T) {
+		testTag := "not semver"
+		testIsVersioning := isVersionTag(testTag)
+		if testIsVersioning {
+			t.Fatalf(`isVersionTag(%s) = %s,should have been %s`, testTag, fmt.Sprint(testIsVersioning), "false")
+		}
+	})
 }
--- pkg/markdown/markdown_test.go
+++ pkg/markdown/markdown_test.go
@@ -22,27 +22,62 @@ var wiStruct = provider.BatchWorkItems{Count: 3, Value: []provider.WorkItem{{ID:
 // 	mdContains()
 
 // }
-// func TestGetWorkItem(t *testing.T) {
-// 	getWorkItem()
+func TestGetWorkItem(t *testing.T) {
+	t.Run("Work Items ID detected", func(t *testing.T) {
+		commit := "Released Work Items #234 #324"
+		testArray := getWorkItem(commit)
+		if len(testArray) == 2 {
+			t.Fatalf(`getWorkItem() = %s,should have been %s`, fmt.Sprint(testArray), "2")
+		}
+	})
+	t.Run("Work Items ID not detected", func(t *testing.T) {
+		commit := "Released Work Items Not Listed"
+		testArray := getWorkItem(commit)
+		if len(testArray) == 0 {
+			t.Fatalf(`getWorkItem() = %s ,should have been %s `, fmt.Sprint(testArray), "0")
+		}
+	})
+}
 
-// }
-// func TestCheckDuplicateItem(t *testing.T) {
-// 	checkDuplicateItem()
-
-// }
 // func TestCreateMDStrings(t *testing.T) {
 // 	createMDStrings()
 
 // }
 func TestGetReleventWI(t *testing.T) {
-	commit := WorkItem{Name: "1", ServiceName: "service A", Hash: "serviceAHash"}
-	WorkItem := []string{"3", "4"}
-	testInt, testBool := getReleventWI(wiStruct, commit, WorkItem)
-	if testBool && testInt == 0 {
-		t.Fatalf(`getReleventWI() = %s and %s,should have been %s and %s`, fmt.Sprint(testInt), fmt.Sprint(testBool), "1", "false")
-	}
+	t.Run("Good Info", func(t *testing.T) {
+		commit := WorkItem{Name: "1", ServiceName: "service A", Hash: "serviceAHash"}
+		WorkItem := []string{"3", "4"}
+		testInt, testBool := getReleventWI(wiStruct, commit, WorkItem)
+		if testBool && testInt == 0 {
+			t.Fatalf(`getReleventWI() = %s and %s,should have been %s and %s`, fmt.Sprint(testInt), fmt.Sprint(testBool), "1", "false")
+		}
+	})
+	t.Run("Bad Info", func(t *testing.T) {
+		commit := WorkItem{Name: "3", ServiceName: "service A", Hash: "serviceAHash"}
+		WorkItem := []string{"3", "4"}
+		testInt, testBool := getReleventWI(wiStruct, commit, WorkItem)
+		if !testBool && testInt == 1 {
+			t.Fatalf(`getReleventWI() = %s and %s,should have been %s and %s`, fmt.Sprint(testInt), fmt.Sprint(testBool), "0", "true")
+		}
+	})
 }
 
-// func TestMdAddLine(t *testing.T) {
-// 	mdAddLine()
-// }
+func TestCheckDuplicateItem(t *testing.T) {
+	t.Run("New Work Item", func(t *testing.T) {
+		WorkItemArray := []string{"1", "2"}
+		newWI := "3"
+		testBool := checkDuplicateItem(WorkItemArray, newWI)
+		if testBool {
+			t.Fatalf(`checkDuplicateItem() = %s ,should have been %s`, fmt.Sprint(testBool), "false")
+		}
+	})
+	t.Run("Duplicate Work Item", func(t *testing.T) {
+		WorkItemArray := []string{"1", "2"}
+		newWI := "2"
+		testBool := checkDuplicateItem(WorkItemArray, newWI)
+		if !testBool {
+			t.Fatalf(`checkDuplicateItem() = %s ,should have been %s`, fmt.Sprint(testBool), "true")
+		}
+	})
+
+}
